00bb2441a4f0d23955141c257fcc78207fb39dd7
Chan ho Moon <minsonyo@naver.com>
2022-01-04 13:13:22 +0900

    BOJ 2742. 기찍 N
    BOJ 2742. 기찍 N

---
+++ 2742.cpp
@@ -0,0 +1,17 @@
+// BOJ 2742 ±âÂï N
+// https://www.acmicpc.net/problem/2742
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	while (n > 0)
+		cout << n-- << '\n';
+
+	return 0;
+}
