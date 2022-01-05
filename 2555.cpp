fd6610bd8eeba49edb88e491ffae2d3eec31a2dd
Chan ho Moon <minsonyo@naver.com>
2022-01-06 01:37:15 +0900

    BOJ 2555. 생일 출력하기
    BOJ 2555. 생일 출력하기

---
+++ 2555.cpp
@@ -0,0 +1,13 @@
+// BOJ 2555. 생일 출력하기
+// https://www.acmicpc.net/problem/2555
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << "3/19" << '\n';
+    return 0;
+}
